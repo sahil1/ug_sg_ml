33a7cd7c5b9c6a12f1fe6299efa0f363b711a3f7
Sahil Maheshwari <maheshwari.sahil1@gmail.com>
2018-12-10 07:56:21 -0800

    Added test and training dataset

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -1,5 +1,5 @@
 const outputs = [];
-const predictionPoint = 300;
+// const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -10,8 +10,26 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
     // Write code here to analyze stuff
-    const bucket = _.chain(outputs)
-        .map(row => [distance(row[0]), row[3]])
+    const testSetSize = 10;
+    const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+
+    let numberCorrect = 0;
+    for (let i = 0; i < testSet.length; i++) {
+        const bucket = knn(trainingSet, testSet[i][0]);
+        console.log(bucket, testSet[i][3]);
+        if (bucket === testSet[i][3]) {
+            numberCorrect++;
+        }
+    }
+
+    console.log('Accuracy:', numberCorrect / testSetSize);
+    // const bucket = knn(outputs);
+    // console.log('Ball will fall into bucket #', bucket);
+}
+
+function knn(data, point) {
+    return _.chain(data)
+        .map(row => [distance(row[0], point), row[3]])
         .sortBy(row => row[0])
         .slice(0, k)
         .countBy(row => row[1])
@@ -21,10 +39,17 @@ function runAnalysis() {
         .first()
         .parseInt()
         .value();
-
-    console.log('Ball will fall into bucket #', bucket);
 }
 
-function distance(point) {
-    return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+    return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+    const shuffled = _.shuffle(data);
+
+    const testSet = _.slice(shuffled, 0, testCount);
+    const trainingSet = _.slice(shuffled, testCount);
+
+    return [testSet, trainingSet];
 }

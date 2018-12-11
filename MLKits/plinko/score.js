4ab404058d06cbd71d4b9c94cc14f3a5a164b871
Sahil <sahil.maheshwari@oracle.com>
2018-12-10 17:13:20 -0800

    Added KNN update

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -1,33 +1,43 @@
 const outputs = [];
 // const predictionPoint = 300;
-const k = 3;
+// const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
     // Ran every time a balls drops into a bucket
     outputs.push([dropPosition, bounciness, size, bucketLabel]);
-    console.log(outputs);
+    // console.log(outputs);
 }
 
 function runAnalysis() {
     // Write code here to analyze stuff
-    const testSetSize = 10;
+    const testSetSize = 50;
     const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
-    let numberCorrect = 0;
-    for (let i = 0; i < testSet.length; i++) {
-        const bucket = knn(trainingSet, testSet[i][0]);
-        console.log(bucket, testSet[i][3]);
-        if (bucket === testSet[i][3]) {
-            numberCorrect++;
-        }
-    }
+    _.range(1, 15).forEach(k => {
+        const accuracy = _.chain(testSet)
+            .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+            .size()
+            .divide(testSetSize)
+            .value();
 
-    console.log('Accuracy:', numberCorrect / testSetSize);
+        console.log('For k of ', k, ' accuracy is', accuracy);
+    });
+
+
+    // let numberCorrect = 0;
+    // for (let i = 0; i < testSet.length; i++) {
+    //     const bucket = knn(trainingSet, testSet[i][0]);
+    //     console.log(bucket, testSet[i][3]);
+    //     if (bucket === testSet[i][3]) {
+    //         numberCorrect++;
+    //     }
+    // }
+    // console.log('Accuracy:', numberCorrect / testSetSize);
     // const bucket = knn(outputs);
     // console.log('Ball will fall into bucket #', bucket);
 }
 
-function knn(data, point) {
+function knn(data, point, k) {
     return _.chain(data)
         .map(row => [distance(row[0], point), row[3]])
         .sortBy(row => row[0])

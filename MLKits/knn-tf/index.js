46848f204b4b54f5819af7c4473587321a8a664f
Sahil Maheshwari <maheshwari.sahil1@gmail.com>
2018-12-11 18:56:22 -0800

    Till end of section 4

--- MLKits/knn-tf/index.js
+++ MLKits/knn-tf/index.js
@@ -3,14 +3,14 @@ const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
 function knn(features, labels, predictionPoint, k) {
-
     const { mean, variance } = tf.moments(features, 0);
     // features.sub(mean).div(variance.pow(0.5));
     const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
 
     return (
         features
-            .sub(mean).div(variance.pow(0.5))
+            .sub(mean)
+            .div(variance.pow(0.5))
             .sub(scaledPrediction)
             .pow(2)
             .sum(1)
@@ -29,7 +29,7 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
     {
         shuffle: true,
         splitTest: 10,
-        dataColumns: ['lat', 'long', 'sqft_lot'],
+        dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
         labelColumns: ['price'],
     }
 );

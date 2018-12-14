50c60b8395f8ac061b6f6fece7f6830ba70482f4
Sahil Maheshwari <maheshwari.sahil1@gmail.com>
2018-12-13 22:15:31 -0800

    Fixed issue with NaN in guesses

--- MLKits/regressions/handwriting-recognition/logistic-regression.js
+++ MLKits/regressions/handwriting-recognition/logistic-regression.js
@@ -107,17 +107,19 @@ class LogisticRegression {
         const cost = tf.tidy(() => {
             const guesses = this.features.matMul(this.weights).softmax();
 
-            const termOne = this.labels.transpose().matMul(guesses.log());
+            const termOne = this.labels
+                .transpose()
+                .matMul(guesses.add(1e-7).log());
 
             const termTwo = this.labels
                 .mul(-1)
                 .add(1)
-                .add(1e-7) //0.00000001 as log of this is not -ve infinity
                 .transpose()
                 .matMul(
                     guesses
                         .mul(-1)
                         .add(1)
+                        .add(1e-7)
                         .log()
                 );
 
